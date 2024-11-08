913f035a40d27b53f5043137c8daf59ce494bc56
richban <rbanyi@me.com>
2024-11-08 12:03:07 +0800

    init project

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Data Engineering Stack";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShellNoCC {
+          packages = with pkgs; [
+            python311
+            postgresql
+            minio
+            minio-client
+            gdal
+          ];
+        };
+      }
+    );
+}
