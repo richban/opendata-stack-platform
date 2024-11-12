51913bdfa774f70eb51f09e417f051854f917a14
richban <rbanyi@me.com>
2024-11-12 16:32:05 +0800

    Added constants

---
+++ opendata_stack_platform_project/opendata_stack_platform/assets/constants.py
@@ -0,0 +1,6 @@
+BUCKET = "datalake"
+TAXI_ZONES_FILE_PATH = "raw/taxi_zones.csv"
+TAXI_TRIPS_TEMPLATE_FILE_PATH = "raw/yellow_taxi_trips/taxi_trips_{}.parquet"
+
+START_DATE = "2009-01-01"
+END_DATE = "2024-08-01"
