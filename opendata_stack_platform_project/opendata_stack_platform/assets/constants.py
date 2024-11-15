02a7659b2a0c91302b04228cb85444851f941e79
richban <rbanyi@me.com>
2024-11-15 18:41:22 +0800

    Update constants

--- opendata_stack_platform_project/opendata_stack_platform/assets/constants.py
+++ opendata_stack_platform_project/opendata_stack_platform/assets/constants.py
@@ -2,5 +2,7 @@ BUCKET = "datalake"
 TAXI_ZONES_FILE_PATH = "raw/taxi_zones.csv"
 TAXI_TRIPS_TEMPLATE_FILE_PATH = "raw/yellow_taxi_trips/taxi_trips_{}.parquet"
 
-START_DATE = "2009-01-01"
+TAXI_TRIPS_RAW_KEY_TEMPLATE = "raw/{dataset_type}/{dataset_type}_tripdata_{partition}.parquet"
+
+START_DATE = "2024-01-01"
 END_DATE = "2024-08-01"
