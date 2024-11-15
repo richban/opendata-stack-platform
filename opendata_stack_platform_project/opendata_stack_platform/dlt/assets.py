fc0cee701fa85a98a88c1b772a3df39bf66f9887
richban <rbanyi@me.com>
2024-11-15 19:18:18 +0800

    Create dependency between dagster and dlt asset

--- opendata_stack_platform_project/opendata_stack_platform/dlt/assets.py
+++ opendata_stack_platform_project/opendata_stack_platform/dlt/assets.py
@@ -1,21 +1,38 @@
-from dagster import AssetExecutionContext
-from dagster_embedded_elt.dlt import DagsterDltResource, dlt_assets
+from collections.abc import Iterable
+from dagster import AssetExecutionContext, AssetKey
+from dagster_embedded_elt.dlt import DagsterDltResource, dlt_assets, DagsterDltTranslator
 import dlt
 from opendata_stack_platform.dlt.sources.yellow_taxi_trip import yellow_taxi_trip
+from opendata_stack_platform.partitions import monthly_partition
+
+
+class YellowTaxiTripDagsterDltTranslator(DagsterDltTranslator):
+    def get_asset_key(self, resource: DagsterDltResource) -> AssetKey:
+        """Overrides asset key to be the dlt resource name."""
+        return AssetKey("yellow_taxi_trip_bronz")
+
+    def get_deps_asset_keys(self, resource: DagsterDltResource) -> Iterable[AssetKey]:
+        yield AssetKey("taxi_trips_file")
 
 
 @dlt_assets(
-    dlt_source=yellow_taxi_trip(partition="2024-01-01"),
+    dlt_source=yellow_taxi_trip(),
     dlt_pipeline=dlt.pipeline(
-        pipeline_name="yellow_taxi_trip_pipeline",
+        pipeline_name="yellow_taxi_trip_bronz_pipeline",
         destination=dlt.destinations.duckdb("../data/nyc_database.duckdb"),
-        dataset_name="yellow_taxi_trip_raw",
+        dataset_name="yellow_taxi_trip_bronz",
         dev_mode=True,
+        progress="log"
     ),
     name="dlt_pipeline",
     group_name="dlt_assets",
+    dagster_dlt_translator=YellowTaxiTripDagsterDltTranslator(),
+    partitions_def=monthly_partition
+
 )
-def dagster_planetview_database_assets(
+def dagster_yellow_taxi_trip_bronz_assets(
     context: AssetExecutionContext, dlt: DagsterDltResource
 ):
-    yield from dlt.run(context=context)
+    partition_key = context.partition_key[:-3]
+    yield from dlt.run(context=context, dlt_source=yellow_taxi_trip(partition_key=partition_key))
+
