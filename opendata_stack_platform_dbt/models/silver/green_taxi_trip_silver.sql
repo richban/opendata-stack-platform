396b7b19a484b8f87dc8dbb783731f95dd529f5a
richban <rbanyi@me.com>
2024-12-17 20:43:15 +0800

    Testing incremental partitioned model

--- opendata_stack_platform_dbt/models/silver/green_taxi_trip_silver.sql
+++ opendata_stack_platform_dbt/models/silver/green_taxi_trip_silver.sql
@@ -1,6 +1,14 @@
--- This model selects all columns from the green_taxi_trip_bronze source
-
-{{ config( materialized='view') }}
+{{ config(
+    materialized='incremental',
+    partition_by={
+        "field": "date_partition",
+        "data_type": "date",
+        "granularity": "month",
+    }
+) }}
 
 select *
-from {{ source('taxi_trips_bronze', 'green_taxi_trip_bronze') }} 
+from {{ source('taxi_trips_bronze', 'green_taxi_trip_bronze') }}
+{% if is_incremental() %}
+where date_partition = CAST('{{ var("date_partition") }}' AS DATE)
+{% endif %}
