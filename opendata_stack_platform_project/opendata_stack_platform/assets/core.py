5b44289f10411bb6e6a64cc0c7ea18f9d0717884
richban <rbanyi@me.com>
2025-02-26 17:06:12 +0100

    Cleanup redundant assets; Fix Zone Files

--- opendata_stack_platform_project/opendata_stack_platform/assets/core.py
+++ opendata_stack_platform_project/opendata_stack_platform/assets/core.py
@@ -1,8 +1,17 @@
+from io import BytesIO
+
 import polars as pl
 import requests
 
-from botocore.exceptions import BotoCoreError, ClientError
-from dagster import AssetExecutionContext, AssetKey, AssetSpec, EnvVar, Failure, asset
+from dagster import (
+    AssetExecutionContext,
+    AssetKey,
+    AssetSpec,
+    EnvVar,
+    MaterializeResult,
+    MetadataValue,
+    asset,
+)
 from dagster_aws.s3 import S3Resource
 
 from opendata_stack_platform.assets import constants
@@ -46,65 +55,13 @@ def derived_asset_from_source(portfolio_real_assets: pl.LazyFrame) -> pl.DataFra
     return filtered_df.collect()
 
 
-@asset(partitions_def=monthly_partition)
-def taxi_trips_file(context: AssetExecutionContext, s3: S3Resource) -> None:
-    """
-    The raw parquet files for the taxi trips dataset. Sourced from the
-         NYC Open Data portal.
-    """
-    partition_key = context.partition_key  # YYYY-MM-DD
-    partition_to_fetch = partition_key[:-3]  # YYYY-MM
-
-    url = (
-        "https://d37ci6vzurychx.cloudfront.net/trip-data/"
-        f"yellow_tripdata_{partition_to_fetch}.parquet"
-    )
-    s3_key = constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(partition_key)
-    s3_bucket = constants.BUCKET
-
-    # Logging the start of the download process
-    context.log.info(
-        "Starting download for partition: " f"{partition_to_fetch} from {url}"
-    )
-
-    try:
-        # Download the file
-        response = requests.get(url)
-        response.raise_for_status()  # Raise an error for HTTP errors
-
-        # Calculate file size in MiB
-        file_size_mib = len(response.content) / (1024 * 1024)
-        context.log.info(
-            "Downloaded data for " f"{partition_to_fetch}, size: {file_size_mib:.2f} MiB"
-        )
-
-        # Upload the file to S3
-        s3.get_client().put_object(Bucket=s3_bucket, Key=s3_key, Body=response.content)
-
-        context.log.info(f"Successfully uploaded {s3_key} to bucket {s3_bucket}")
-
-    except requests.exceptions.RequestException as e:
-        context.log.error(
-            "Failed to download file for partition "
-            f"{partition_to_fetch} from {url}: {e}"
-        )
-        raise Failure(f"Download error for partition {partition_to_fetch}: {e!s}") from e
-
-    except (BotoCoreError, ClientError) as e:
-        context.log.error(
-            "Failed to upload file to S3 for partition "
-            f"{partition_to_fetch}, key {s3_key}: {e}"
-        )
-        raise Failure(f"S3 upload error for partition {partition_to_fetch}: {e!s}") from e
-
-
 @asset
 def taxi_zones_file(s3: S3Resource) -> None:
     """
     The raw CSV file for the taxi zones dataset. Sourced from the NYC Open Data portal.
     """
     raw_taxi_zones = requests.get(
-        "https://data.cityofnewyork.us/api/views/755u-8jsi/rows.csv?accessType=DOWNLOAD"
+        "https://community-engineering-artifacts.s3.us-west-2.amazonaws.com/dagster-university/data/taxi_zones.csv"
     )
 
     s3_key = constants.TAXI_ZONES_FILE_PATH
@@ -112,6 +69,10 @@ def taxi_zones_file(s3: S3Resource) -> None:
     s3.get_client().put_object(
         Bucket=constants.BUCKET, Key=s3_key, Body=raw_taxi_zones.content
     )
+
+    num_rows = len(pl.read_csv(BytesIO(raw_taxi_zones.content)))
+
+    return MaterializeResult(metadata={"Number of records": MetadataValue.int(num_rows)})
 
 
 @asset(partitions_def=monthly_partition)
