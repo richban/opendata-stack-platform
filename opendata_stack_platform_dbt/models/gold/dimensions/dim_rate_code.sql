e6715cbf6d8ee9944eb251ac5104dea789fc0ae9
richban <rbanyi@me.com>
2025-03-22 20:31:04 +0100

    Fix dim_rate_code

--- opendata_stack_platform_dbt/models/gold/dimensions/dim_rate_code.sql
+++ opendata_stack_platform_dbt/models/gold/dimensions/dim_rate_code.sql
@@ -11,34 +11,34 @@ Rate codes vary by taxi type, with Yellow and Green taxis sharing a similar stru
 
 with rate_code_mapping as (
     select
-        rate_code_key,
+        rate_code_id,
         case
-            when rate_code_key = 1 then 'Standard rate'
-            when rate_code_key = 2 then 'JFK'
-            when rate_code_key = 3 then 'Newark'
-            when rate_code_key = 4 then 'Nassau or Westchester'
-            when rate_code_key = 5 then 'Negotiated fare'
-            when rate_code_key = 6 then 'Group ride'
-            when rate_code_key = 99 then 'Unknown'
+            when rate_code_id = 1 then 'Standard rate'
+            when rate_code_id = 2 then 'JFK'
+            when rate_code_id = 3 then 'Newark'
+            when rate_code_id = 4 then 'Nassau or Westchester'
+            when rate_code_id = 5 then 'Negotiated fare'
+            when rate_code_id = 6 then 'Group ride'
+            when rate_code_id = 99 then 'Unknown'
             else 'Unknown'
         end as rate_code_desc
     from (
         -- Get all distinct ratecode_id values from Yellow Taxi
-        select distinct coalesce(cast(ratecode_id as int), 99) as rate_code_key
+        select distinct coalesce(cast(ratecode_id as int), 99) as rate_code_id
         from {{ source('silver_yellow', 'yellow_taxi_trip') }}
 
         union
 
         -- Get all distinct ratecode_id values from Green Taxi
-        select distinct coalesce(cast(ratecode_id as int), 99) as rate_code_key
+        select distinct coalesce(cast(ratecode_id as int), 99) as rate_code_id
         from {{ source('silver_green', 'green_taxi_trip') }}
     )
 ),
 
 final as (
     select
-        rate_code_key,
-        rate_code_key as rate_code_id,
+        rate_code_id as rate_code_key,
+        rate_code_id,
         rate_code_desc,
         null as valid_to,
         true as is_current,
