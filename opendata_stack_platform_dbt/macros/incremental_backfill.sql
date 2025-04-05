b2e7a98f3c61d60f82ea0d2d48da425f5c6a1fd7
richban <rbanyi@me.com>
2025-04-05 15:45:41 +0200

    Add incremental backfill macro for DBT models
    - Introduced a new SQL macro to handle incremental backfilling based on specified date ranges or normal incremental behavior.
    - The macro allows for conditional processing of data based on the presence of a backfill start date variable.

---
+++ opendata_stack_platform_dbt/macros/incremental_backfill.sql
@@ -0,0 +1,20 @@
+{% macro incremental_backfill(date_column, taxi_type=none) %}
+    {% if is_incremental() and not var('backfill_start_date', false) %}
+        -- Normal incremental behavior - only process new data
+        and date_trunc('month', {{ date_column }}) >= (
+            select
+                coalesce(
+                    date_trunc('month', max(_incremental_timestamp)),
+                    '2000-01-01'::date
+                )
+            from {{ this }}
+            {% if taxi_type is not none %}
+            where taxi_type = '{{ taxi_type }}'
+            {% endif %}
+        )
+    {% elif is_incremental() and var('backfill_start_date', false) %}
+        -- Backfill behavior - process specified date range
+        and date_trunc('month', {{ date_column }}) >= cast('{{ var("backfill_start_date") }}' as date)
+        and date_trunc('month', {{ date_column }}) <= cast('{{ var("backfill_end_date") }}' as date)
+    {% endif %}
+{% endmacro %} 
