12a17b121bd7ee27f61275a23d4688c1c73218ba
richban <rbanyi@me.com>
2025-08-15 23:09:01 +0200

    Fix  importing asset definitions

--- dagster-workspace/team_data_platform/data_platform/src/data_platform/defs/sqlmesh/definitions.py
+++ dagster-workspace/team_data_platform/data_platform/src/data_platform/defs/sqlmesh/definitions.py
@@ -3,11 +3,11 @@ from dagster import (
     Definitions,
 )
 
-from data_platform.defs.sqlmesh import assets
+from data_platform.defs.sqlmesh.assets import sqlmesh_project
 from data_platform.defs.sqlmesh.resources import sqlmesh_resource
 
 defs = Definitions(
-    assets=[assets],
+    assets=[sqlmesh_project],
     resources={
         "sqlmesh": sqlmesh_resource,
     },
