4d21b6b3140a6cb8365709d3b046f3350c137c60
richban <rbanyi@me.com>
2025-09-16 17:12:17 +0200

    Moved duckdb resource
    DuckSafeDLT resource, the needed DuckDB resource, DLT.

--- dagster-workspace/team_data_platform/data_platform/src/data_platform/defs/taxi/definitions.py
+++ dagster-workspace/team_data_platform/data_platform/src/data_platform/defs/taxi/definitions.py
@@ -6,6 +6,8 @@ from dagster import (
 from dagster_aws.s3 import S3Resource
 
 from data_platform.defs.taxi import assets
+from data_platform.defs.taxi.resources import duckdb_resource
+
 
 s3_resource = S3Resource(
     aws_access_key_id=EnvVar("AWS_ACCESS_KEY_ID"),
@@ -17,5 +19,6 @@ defs = Definitions(
     assets=load_assets_from_modules([assets]),
     resources={
         "s3": s3_resource,
+        "duckdb_resource": duckdb_resource,
     },
 )
