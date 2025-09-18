ae395a729f6bac2089f4f199e9412d019968d466
richban <rbanyi@me.com>
2025-09-18 09:32:46 +0200

    Update dbt definitions to comment out asset loading and duplicate dev profile configuration in profiles.yml for improved clarity and consistency.

--- dagster-workspace/team_data_platform/data_platform/src/data_platform/defs/dbt/definitions.py
+++ dagster-workspace/team_data_platform/data_platform/src/data_platform/defs/dbt/definitions.py
@@ -7,7 +7,7 @@ from data_platform.defs.dbt import assets
 from data_platform.defs.dbt.resources import dbt_resource
 
 defs = Definitions(
-    assets=load_assets_from_modules([assets]),
+    # assets=load_assets_from_modules([assets]),
     resources={
         "dbt": dbt_resource,
     },
