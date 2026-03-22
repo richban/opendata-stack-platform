0339348244e2ccbf879f40cf86a0d7e4ff53e332
richban <rbanyi@me.com>
2026-03-22 14:30:17 +0100

    fix: component resolution

--- dagster-workspace/projects/physical_risk_impact/src/physical_risk_impact/definitions.py
+++ dagster-workspace/projects/physical_risk_impact/src/physical_risk_impact/definitions.py
@@ -1,15 +1,1 @@
-import warnings
-
-import dagster as dg
-
-from dagster._utils.warnings import BetaWarning, PreviewWarning
-
-import physical_risk_impact.defs
-
-warnings.filterwarnings("ignore", category=PreviewWarning)
-warnings.filterwarnings("ignore", category=BetaWarning)
-
-
-defs = dg.Definitions.merge(
-    dg.components.load_defs(physical_risk_impact.defs),
-)
+from physical_risk_impact.defs.definitions import defs
